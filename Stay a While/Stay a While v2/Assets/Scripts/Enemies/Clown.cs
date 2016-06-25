e084ca4312c52914e496f7995aa1e89ad9f617dd
Thomas Pryde <thomas.pryde@hotmail.com>
2016-06-25 00:36:12 -0400

    - Created BaseEnemy and Clown Script

---
+++ Stay a While/Stay a While v2/Assets/Scripts/Enemies/Clown.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clown : BaseEnemy
+{
+    [SerializeField]
+    private float attackDamage;
+
+    protected override void Moving()
+    {
+        base.Moving();
+
+    }
+
+    protected override void Attacking()
+    {
+        base.Attacking();
+
+    }
+
+    protected override void Died()
+    {
+        base.Died();
+
+    }
+
+    protected override void Spawned()
+    {
+        base.Spawned();
+        ChangeState(State.Moving);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        float healthPCT = (healthMAX / health) * 100;
+        if (healthPCT > 75) { }
+        else if (healthPCT > 50) { }
+        else if (healthPCT > 25) { }
+        else { }
+    }
+}
