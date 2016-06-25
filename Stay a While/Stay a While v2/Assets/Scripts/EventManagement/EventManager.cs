0398a58f7a65e76701348d7dfe2aa8b0a61fb031
Quentin Bellay <bellay.quentin@gmail.com>
2016-06-25 00:01:16 -0400

    Added event manager. (plus a test case for an event listener / sender)

---
+++ Stay a While/Stay a While v2/Assets/Scripts/EventManagement/EventManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public delegate void GameEventDelegate(EventData eventData);
+
+public class EventManager
+{
+    static EventManager s_Instance;
+
+    public static EventManager Instance
+    {
+        get
+        {
+            if(s_Instance == null)
+            {
+                s_Instance = new EventManager();
+            }
+
+            return s_Instance;
+        }
+    }
+
+    Dictionary<System.Type, GameEventDelegate> m_EventDelegates;
+
+	// Use this for initialization
+	EventManager ()
+    {
+        m_EventDelegates = new Dictionary<System.Type, GameEventDelegate>();
+	}
+
+    public void RegisterListener(System.Type type, GameEventDelegate listener)
+    {
+        if (!m_EventDelegates.ContainsKey(type))
+        {
+            m_EventDelegates.Add(type, listener);
+        }
+        else
+        {
+            m_EventDelegates[type] += listener;
+        }
+    }
+
+    public void DeregisterListener(System.Type type, GameEventDelegate listener)
+    {
+        if(m_EventDelegates.ContainsKey(type))
+        {
+            m_EventDelegates[type] -= listener;
+        }
+    }
+
+    public void SendEvent(EventData eventData)
+    {
+        System.Type type = eventData.GetType();
+
+        if (m_EventDelegates.ContainsKey(type))
+        {
+            m_EventDelegates[type](eventData);
+        }
+    }
+}
---
+++ Stay a While/Stay a While v2/Assets/Scripts/EventManagement/TestEvent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEventData : EventData
+{
+    public int m_Whatever;
+}
+
+public class TestEvent : MonoBehaviour
+{
+    public int m_Whatever;
+
+    GameEventDelegate m_TestDelegate;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_TestDelegate = new GameEventDelegate(ReceiveTestEvent);
+
+        EventManager.Instance.RegisterListener(typeof(TestEventData), m_TestDelegate);
+	}
+
+    // For clean up
+    void OnDestroy()
+    {
+        EventManager.Instance.DeregisterListener(typeof(TestEventData), m_TestDelegate);
+    }
+
+    void ReceiveTestEvent(EventData data)
+    {
+        Debug.Log(((TestEventData)data).m_Whatever);
+    }
+
+    [ContextMenu("SendEvent")]
+    void SendEvent()
+    {
+        TestEventData data = new TestEventData();
+        data.m_Whatever = m_Whatever;
+
+        EventManager.Instance.SendEvent(data);
+    }
+}
