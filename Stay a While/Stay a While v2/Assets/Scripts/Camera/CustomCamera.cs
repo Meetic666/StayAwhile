22fa3b2dc194da972871f54e9aa8930499746380
Thomas Pryde <thomas.pryde@hotmail.com>
2016-06-25 17:21:41 -0400

    - Created Custom Camera script for shaking and focusing on player(s)

--- Stay a While/Stay a While v2/Assets/Scripts/Camera/CustomCamera.cs
+++ Stay a While/Stay a While v2/Assets/Scripts/Camera/CustomCamera.cs
@@ -1,15 +1,89 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CustomCamera : MonoBehaviour {
+public class CustomCamera : MonoBehaviour
+{
+    private bool shaking;
+    private bool lerping;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private float duration;
+    private float force;
+    private Vector3 axis;
+    private int playerIndex;
+    private float movement;
+    
+    public void CameraShake(float force = 1)
+    {
+        axis = Vector3.one;
+        axis.z = 0;
+        duration = 10;
+        this.force = force;
+
+        StartCoroutine(cameraShake_cr());
+    }
+
+    public void CameraShake(Vector3 axis, float force)
+    {
+        this.force = force;
+        this.axis = axis;
+        this.axis.z = 0;
+        duration = 1;
+
+        StartCoroutine(cameraShake_cr());
+    }
+
+    public void CameraShake(Vector3 axis, float force, float duration)
+    {
+        this.force = force;
+        this.axis = axis;
+        this.axis.z = 0;
+        this.duration = duration;
+
+        StartCoroutine(cameraShake_cr());
+    }
+
+    private IEnumerator cameraShake_cr()
+    {
+        if (!shaking)
+        {
+            shaking = true;
+            Vector3 origin = this.transform.position;
+            Vector3 pos = Vector3.zero;
+            while (duration > 0)
+            {
+                if (lerping) { force /= 2; }
+                axis *= force;
+                pos.x = Random.Range(-axis.x, axis.x);
+                pos.y = Random.Range(-axis.y, axis.y);
+                pos.z = Random.Range(-axis.z, axis.z);
+
+                this.transform.position = origin + pos;
+
+                duration -= Time.deltaTime;
+                yield return null;
+            }
+            shaking = false;
+        }
+    }
+
+    public void PlayerFocus(int playerIndex, float movement = 0.1f)
+    {
+        this.playerIndex = playerIndex;
+        this.movement = movement;
+        StartCoroutine(playerFocus_cr());
+    }
+
+    private IEnumerator playerFocus_cr()
+    {
+        Vector3 origin = this.transform.position;
+        lerping = true;
+        while(duration > 0)
+        {
+            this.transform.position = Vector3.Lerp(origin, ObjectSingleton.Instance.playerList[playerIndex].transform.position, movement);
+
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+        lerping = false;
+    }
 }
