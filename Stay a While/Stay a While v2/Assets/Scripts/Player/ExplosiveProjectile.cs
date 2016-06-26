5469a25f272239272ad8d47d03c6757343d680f9
Quentin Bellay <bellay.quentin@gmail.com>
2016-06-26 09:31:26 -0400

    Added explosion behaviour and sound

--- Stay a While/Stay a While v2/Assets/Scripts/Player/ExplosiveProjectile.cs
+++ Stay a While/Stay a While v2/Assets/Scripts/Player/ExplosiveProjectile.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ExplosiveProjectile : BaseProjectile 
 {
     public float ExplosionRadius = 5.0f;
+    public float m_ExplosionTime = 1.0f;
     public float MinDetTime = 0.5f;
     float defaultMinDetTime;
     public float TimeToDetonate = 3.0f;
@@ -12,11 +13,18 @@ public class ExplosiveProjectile : BaseProjectile
     float defaultSpeed = 0.0f;
     Animator animator;
 
+    float m_ExplosionTimer;
+
+    ShootEventData m_EventData;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         defaultSpeed = projectileSpeed;
         defaultTimeToDetonate = TimeToDetonate;
+
+        m_EventData = new ShootEventData();
+        m_EventData.m_WeaponName = "Explosion";
     }
 
     void OnEnable()
@@ -45,29 +53,46 @@ public class ExplosiveProjectile : BaseProjectile
         }
         else
         {
-
-            RaycastHit2D hit = Physics2D.CircleCast(gameObject.transform.position, Radius, transform.up, 0.1f, maskToHit);
-            if (hit == true || TimeToDetonate < 0.0f)
+            if(m_ExplosionTimer <= 0.0f)
             {
-                animator.SetTrigger("Death");
-                Vector2 point = Vector2.zero;
-                if (hit == true)
-                    point = hit.point;
-                else
-                    point = gameObject.transform.position;
-
-
-                Collider2D[] colls = Physics2D.OverlapCircleAll(point, ExplosionRadius, maskToHit);
-                for (int i = 0; i < colls.Length; i++)
+                if (TimeToDetonate < 0.0f)
                 {
+                    animator.SetTrigger("Death");
 
-                    float Damage = DamageToDeal / (Vector2.Distance(point, colls[i].transform.position) < 1 ? 1 : Vector2.Distance(point, colls[i].transform.position));
-                    if (colls[i].GetComponent<BaseEnemy>() != null)
-                        colls[i].GetComponent<BaseEnemy>().DealDamage(Damage);
-                    if (colls[i].GetComponent<BasePlayer>() != null)
-                        colls[i].GetComponent<BasePlayer>().Damage(Damage);
+                    RaycastHit2D hit = Physics2D.CircleCast(gameObject.transform.position, Radius, transform.up, 0.1f, maskToHit);
+                    if (hit == true)
+                    {
+                        Vector2 point = Vector2.zero;
+                        if (hit == true)
+                            point = hit.point;
+                        else
+                            point = gameObject.transform.position;
+
+
+                        Collider2D[] colls = Physics2D.OverlapCircleAll(point, ExplosionRadius, maskToHit);
+                        for (int i = 0; i < colls.Length; i++)
+                        {
+                            float Damage = DamageToDeal / (Vector2.Distance(point, colls[i].transform.position) < 1 ? 1 : Vector2.Distance(point, colls[i].transform.position));
+                            if (colls[i].GetComponent<BaseEnemy>() != null)
+                                colls[i].GetComponent<BaseEnemy>().DealDamage(Damage);
+                            if (colls[i].GetComponent<BasePlayer>() != null)
+                                colls[i].GetComponent<BasePlayer>().Damage(Damage);
+                        }
+                    }
+
+                    EventManager.Instance.SendEvent(m_EventData);
+
+                    m_ExplosionTimer = m_ExplosionTime;
                 }
-                
+            }
+            else
+            {
+                m_ExplosionTimer -= Time.deltaTime;
+
+                if (m_ExplosionTimer <= 0.0f)
+                {
+                    Death();
+                }
             }
         }
     }
