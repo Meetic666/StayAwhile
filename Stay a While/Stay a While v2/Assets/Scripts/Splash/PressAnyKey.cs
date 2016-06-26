81f5544c154b875d315a19e04ee229c1e6a972e4
Quentin Bellay <bellay.quentin@gmail.com>
2016-06-26 08:05:26 -0400

    Added splash screen

---
+++ Stay a While/Stay a While v2/Assets/Scripts/Splash/PressAnyKey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PressAnyKey : MonoBehaviour
+{
+    public string m_SceneToLoad;
+
+    bool m_IsLoading;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(!m_IsLoading
+            && Input.anyKey)
+        {
+            m_IsLoading = true;
+
+            SceneManager.LoadSceneAsync(m_SceneToLoad);
+        }
+	}
+}
